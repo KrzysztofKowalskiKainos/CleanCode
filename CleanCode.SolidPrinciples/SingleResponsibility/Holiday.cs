4d8288975c8450d9012de58b2799132261467e73
ahnes86 <akowalska12@gmail.com>
2015-05-30 13:52:58 +0200

    poprawki 1

---
+++ CleanCode.SolidPrinciples/SingleResponsibility/Holiday.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CleanCode.SolidPrinciples
+{
+    class Holiday
+    {
+        public string HolidayName { get; private set; }
+
+        public Holiday(string holidayName)
+        {
+            HolidayName = holidayName;
+        }
+    }
+}
