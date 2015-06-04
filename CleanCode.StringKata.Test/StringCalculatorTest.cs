484767e0c9eca137a3e8d4f28280a7a9163fc186
aaroslanow <Adam.Aroslanow@gmail.com>
2015-06-04 12:23:25 +0200

    TDD KATA1 added

--- CleanCode.StringKata.Test/StringCalculatorTest.cs
+++ CleanCode.StringKata.Test/StringCalculatorTest.cs
@@ -1,15 +1,89 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace CleanCode.StringKata.Test
 {
     [TestClass]
     public class StringCalculatorTest
     {
+        private StringCalculator calculator =new StringCalculator();
         [TestMethod]
-        public void When_CalculateIsCalled_TestNothing()
+        public void Test_Empty_Parameter()
         {
-            Assert.Fail();
+
+            Assert.AreEqual(0, calculator.Add(""));
         }
+        [TestMethod]
+        public void Test_one_Parameter()
+        {
+            Assert.AreEqual(1, calculator.Add("1"));
+        }
+
+        [TestMethod]
+        public void Test_two_Parameters()
+        {
+            Assert.AreEqual(3, calculator.Add("1,2"));
+        }
+
+        [TestMethod]
+        public void Test_many_Parameters()
+        {
+            Assert.AreEqual(6, calculator.Add("1,2\n3"));
+        }
+
+
+        [TestMethod]
+        public void Test_many_Parameters_with_declared_separator()
+        {
+            Assert.AreEqual(6, calculator.Add("//;\n1;2;3"));
+        }
+
+        [TestMethod]
+        [ExpectedException( typeof(Exception))]
+        public void Test_negatives()
+        {
+            try
+            {
+                calculator.Add("//;\n1;-2;3");
+            }catch(Exception ex)
+            {
+                Assert.AreEqual("negatives found : -2", ex.Message);
+                throw;
+            }
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Test_multi_negatives()
+        {
+            try
+            {
+                calculator.Add("//;\n1;-2;-3");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("negatives found : -2,-3", ex.Message);
+                throw;
+            }
+
+        }
+        [TestMethod]
+        public void Test_many_Parameters_with_declared_separator1()
+        {
+            Assert.AreEqual(6, calculator.Add("//[**]\n1**2**3"));
+        }
+
+        [TestMethod]
+        public void Test_many_Parameters_with_declared_separator2()
+        {
+            Assert.AreEqual(6, calculator.Add("//[**][*]\n1**2*3"));
+        }
+
+        [TestMethod]
+        public void Test_many_Parameters_with_declared_separator3()
+        {
+            Assert.AreEqual(6, calculator.Add("//[**][;]\n1**2;3"));
+        }
+
     }
 }
--- CleanCode.StringKata/StringCalculator.cs
+++ CleanCode.StringKata/StringCalculator.cs
@@ -8,9 +8,117 @@ namespace CleanCode.StringKata
 {
     public class StringCalculator
     {
-        public int Calculate(string input)
+        public int Add(string input)
         {
-            return 0;
+            List<string> negatives  = new List<string>();
+            string[] separator =new string[]{","};
+
+            if (input.Length == 0)
+                return 0;
+
+            if (checkDeclaredSeparator(input))
+            {
+                separator = getSeparator(input);
+                removeSeparateDeclaration(ref input);
+            }
+            else
+            {
+            
+                if (input.Split('\n').Length> 0)
+                    input = input.Replace('\n', ',');
+            }
+                
+            if (isOnlyOneElement(input, separator))
+                return computeOneElement(input);
+            else
+            {
+                int result = computeMultiElements(input,separator,ref negatives);
+
+                try
+                {
+                    checkNegatives(negatives);
+
+                }catch(Exception ex)
+                {
+                    throw ex;
+                }
+
+
+                return result;
+            }
+ 
         }
+
+
+        public string[] getSeparator(string input)
+        {
+            if (input.Contains('['))
+            {
+                input = input.Substring(3, input.IndexOf('\n') - 4);
+              //  input = input.Replace("]", string.Empty);
+
+                string[] separator = input.Split(new string[]{"]["},StringSplitOptions.None);
+                return separator;
+            }
+            else
+            {
+                string[] separator = new string[] { input.Substring(2, input.IndexOf('\n') - 2) };
+                return separator;
+            }
+        }
+
+        public bool checkDeclaredSeparator(string input)
+        {
+            if (input.Length > 2)
+            {
+                if (input.Substring(0, 2) == "//")
+                    return true;
+            }
+                return false;
+        }
+        public void removeSeparateDeclaration(ref string input)
+        {
+            input = input.Substring(input.IndexOf('\n') + 1);
+        }
+        public int computeOneElement(string input)
+        {
+                if (int.Parse(input) > 1000)
+                    return 0;
+                else
+                    return int.Parse(input);
+        }
+        public bool isOnlyOneElement(string input, string[] separator)
+        {
+            foreach (string sep in separator)
+            {
+                if (input.Contains(sep))
+                    return false;
+            }
+            return true;
+        }
+        public int computeMultiElements(string input, string[] separator, ref List<string> negatives)
+        {
+            string[] splittedInput = input.Split(separator,StringSplitOptions.None);
+
+            int result = 0;
+            foreach (string number in splittedInput)
+            {
+                if (int.Parse(number) < 0)
+                    negatives.Add(number);
+                else
+                {
+                    if (int.Parse(number) <= 1000)
+                        result += int.Parse(number);
+                }
+            }
+            return result;
+        }
+        public void checkNegatives(List<string> negatives)
+        {
+           if (negatives.Count()>0)
+               throw new Exception("negatives found : " + string.Join(",", negatives.ToArray()));
+        }
+
     }
+  
 }
